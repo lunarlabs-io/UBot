b1a0bad14590f42d54e7ca6faf02b29185d94352
Uni <uni@uniproto.me>
2019-05-17 20:47:11 -0400

    Added server owner ID to sinfo

--- commands/sinfo.js
+++ commands/sinfo.js
@@ -8,6 +8,7 @@ exports.run = (client, message, args) => {
         .addField("Server name", message.guild.name)
         .addField("Server ID", message.guild.id)
         .addField("Server Owner", message.guild.owner)
+        .addField("Server Owner ID", message.guild.owner.id)
         .addField("Server was made on", message.guild.createdAt)
         .addField("You joined on", message.member.joinedAt)
         .addField("Total members of the guild", message.guild.memberCount);
