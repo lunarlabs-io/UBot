e2f5ccbb7fbf216e98e6e403d95bc89f5392c978
Uni Nugiolu <uni@uniproto.me>
2019-05-26 20:18:58 -0400

    Changed default prefix for help commands - ubot! -> u!
    The bot's prefix has been changed in production! This commit reflects
    those changes.

--- commands/help.js
+++ commands/help.js
@@ -9,29 +9,29 @@ exports.run = (client, message, args) => {
             description: "Here you will be able to see all the commands and what they do.",
             fields: [{
                 name: "Moderation",
-                value: "ubot!ban - Lets you ban a user. \n ubot!kick - lets you kick a user. \n ubot!report - Report a user."
+                value: "u!ban - Lets you ban a user. \n u!kick - lets you kick a user. \n u!report - Report a user."
             },
                 {
                     name: "Misc",
-                    value: "ubot!ping - Lets you check the bot's latency. \n ubot!sinfo - Check server information. \n ubot!uinfo - Shows info about the sender"
+                    value: "u!ping - Lets you check the bot's latency. \n u!sinfo - Check server information. \n u!uinfo - Shows info about the sender"
                 },
                 {
                     name: "Message Manipulation",
-                    value: "ubot!purge - Lets you purge messages."
+                    value: "u!purge - Lets you purge messages."
                 },
                 {
                     name: "Images",
-                    value: "ubot!avatar - This command shows the sender's avatar."
+                    value: "u!avatar - This command shows the sender's avatar."
                 },
                 {
                     name: "Furry",
-                    value: "ubot!yiff - Coming soon. \n ubot!awoo - DON'T DO THAT! WHATEVER YOU DO!"
+                    value: "u!yiff - Coming soon. \n u!awoo - DON'T DO THAT! WHATEVER YOU DO!"
                 }
             ],
             timestamp: new Date(),
             footer: {
                 icon_url: client.user.avatarURL,
-                text: "UBot"
+                text: "Command handled by UBot"
             }
         }
     })};
