c4fa9b5d6b2395078fea37c689fe473b547cbac2
Uni Nugiolu <1-uni@users.noreply.gitlab.uniproto.me>
2019-06-10 20:59:37 -0400

    fixing this if statement hell

--- commands/uni.js
+++ commands/uni.js
@@ -1,20 +1,13 @@
 exports.run = (client, message, args) => {
     var Discord = require('discord.js');
     var username = message.author.username;
-    if (message.member.displayName === "Uni") {
+    if (message.member.displayName === "Uni" || username === "Uni") {
         return message.channel.send({embed: {
                 color: 3447003,
                 title: ":white_check_mark:",
-                description: "Your nickname is Uni! #UniSquad"
+                description: "Your nickname or username is Uni! #UniSquad"
             }
         })}
-    if (username === "Uni") {
-    return message.channel.send({
-        embed: {
-            color: 3447003,
-            title: ":white_check_mark:",
-            description: "Your username is Uni! #UniSquad!"
-        }})}
         else {
         return message.channel.send({
             embed: {
