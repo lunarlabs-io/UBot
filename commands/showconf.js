ab0920ff77c6bbee53dfab202072cf0f9fe3e29c
Uni Nugiolu <1-uni@users.noreply.gitlab.uniproto.me>
2019-06-23 16:49:44 -0400

    Upload New File

---
+++ commands/showconf.js
@@ -0,0 +1,18 @@
+exports.run = async (client, message) => {
+  const serverDefault = {
+    prefix: "u!",
+    modlogChannel: "mod-log",
+    modRole: "Moderator",
+    adminRole: "Administrator",
+    ownerRole: "Owner",
+    welcomeChannel: "welcome",
+    welcomeEn: "false",
+    welcomeMessage: "Welcome to the server {{user}}, We hope you enjoy your stay here!"
+  }
+const guildConf = client.settings.ensure(message.guild.id, serverDefault);
+  let configProps = Object.keys(guildConf).map(prop => {
+    return `${prop}  :  ${guildConf[prop]}\n`;
+  });
+  message.channel.send(`The following are the server's current configuration:
+  \`\`\`${configProps}\`\`\``);
+  }
