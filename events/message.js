d5fda4013c405978c7ec57793c6a9d027a35882f
Uni Nugiolu <1-uni@users.noreply.gitlab.uniproto.me>
2019-06-23 16:48:52 -0400

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,4 +1,6 @@
+var Discord = require('discord.js');
 module.exports = (client, message) => {
+    // eslint-disable-next-line no-unused-vars
     const clean = text => {
         if (typeof(text) === "string")
             return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
@@ -7,9 +9,19 @@ module.exports = (client, message) => {
     }
     // Ignore all bots
     if (message.author.bot) return;
-
+    const serverDefault = {
+        prefix: "u!",
+        modlogChannel: "mod-log",
+        modRole: "Moderator",
+        adminRole: "Administrator",
+        ownerRole: "Owner",
+        welcomeChannel: "welcome",
+        welcomeEn: "false",
+        welcomeMessage: "Welcome to the server {{user}}, We hope you enjoy your stay here!"
+      }
+    const guildConf = client.settings.ensure(message.guild.id, serverDefault);
     // Ignore messages not starting with the prefix (in config.json)
-    if (message.content.indexOf(client.config.prefix) !== 0) return;
+    if(message.content.indexOf(guildConf.prefix) !== 0) return;
     
     // Ignore all DMs
     if (message.channel instanceof Discord.DMChannel) return message.channel.send({embed: {
@@ -19,8 +31,8 @@ module.exports = (client, message) => {
 }});
 
     // Our standard argument/command name definition.
-    const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
-    const command = args.shift().toLowerCase();
+    const args = message.content.split(/\s+/g);
+    const command = args.shift().slice(guildConf.prefix.length).toLowerCase();
 
     // Grab the command data from the client.commands Enmap
     const cmd = client.commands.get(command);
