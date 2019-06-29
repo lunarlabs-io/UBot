e73ebaed1fdb2e46b7fd4d550bb9eea7eab325d1
Banhammer151 <careypjr1993@gmail.com>
2019-06-29 11:03:07 -0500

    Update join/leave log

--- uBot/events/guildCreate.js
+++ uBot/events/guildCreate.js
@@ -1,6 +1,6 @@
 exports.run = async (guild) => {
   const now = Date.now();
   console.log(`uBot has joined ${guild.name}` );
-  this.client.guilds.get("460268770758492170").channels.get("592473392888676373").send(`uBot has Joined ${guild.name} at ${now}`);
+  this.client.guilds.get("555894246717259797").channels.get("594557392541188096").send(`uBot has Joined ${guild.name} at ${now}`);
 };
   
