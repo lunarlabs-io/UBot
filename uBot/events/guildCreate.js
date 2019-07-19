b34ad9802dfc0fa759b5e20c5a934440e3e02b38
Uni Nugiolu <uni@uniproto.me>
2019-07-19 18:00:06 -0400

    - Unexpected }

--- uBot/events/guildCreate.js
+++ uBot/events/guildCreate.js
@@ -8,6 +8,5 @@ exports.run = async (guild) => {
   const now = Date.now();
   console.log(`uBot has joined ${guild.name}` );
   this.client.guilds.get("555894246717259797").channels.get("594557392541188096").send(`uBot has Joined ${guild.name} at ${now}`);
-        )
 }
   
