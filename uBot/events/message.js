83701c21719b6d35b9064d6f235503ee5f66e106
Uni Nugiolu <uni@uniproto.me>
2019-07-19 23:15:11 -0400

    + nickname mention to info msg

--- uBot/events/message.js
+++ uBot/events/message.js
@@ -2,7 +2,7 @@ exports.run = async (msg) => {
 
   if (msg.author.bot) return;
 
-  if (msg.content.startsWith("<@"+ msg.client.user.id +">") && !msg.content.split(" ").slice(1).join(" ")) {
+  if (msg.content.startsWith("<@"+ msg.client.user.id +">") || (msg.content.startsWith("<@!"+ msg.client.user.id +">")  && !msg.content.split(" ").slice(1).join(" "))) {
     msg.channel.send(":wave: | Heya, my name is uBot.\nFor help, use `@uBot help`\nFor info, use `@uBot info`");
   }
 }
