b40268f2fa2754c726e4cceaf26a0d217d7b1a93
Feozeo <feo@protogen.dev>
2019-10-26 08:41:15 -0400

    why did i bother updating a meme command

--- uBot/commands/misc/uni.js
+++ uBot/commands/misc/uni.js
@@ -2,11 +2,11 @@ const { Command } = require("discord.js-commando");
 class UniCommand extends Command {
   constructor(client) {
     super(client, {
-      name: "uni",
+      name: "feo",
       aliases: [],
       group: "misc",
-      memberName: "uni",
-      description: "Checks if your username is 'Uni'",
+      memberName: "feo",
+      description: "Checks if your username is 'Feo'",
       ownerOnly: false,
       guildOnly: false
 
@@ -15,12 +15,12 @@ class UniCommand extends Command {
   async run(msg) {
     //var Discord = require("discord.js");
     var username = msg.author.username;
-    if (msg.member.displayName === "Uni" || username === "Uni") {
+    if (username === "Feo") {
       return msg.channel.send({
         embed: {
           color: 3447003,
           title: ":white_check_mark:",
-          description: "Your nickname or username is Uni! #UniSquad"
+          description: "Your nickname or username is Feo!"
         }
       });
     }
@@ -29,7 +29,7 @@ class UniCommand extends Command {
         embed: {
           color: 3447003,
           title: ":x:",
-          description: "Your name is not Uni!"
+          description: "Your name is not Feo!"
         }
       });
     }
