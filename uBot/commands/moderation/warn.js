40f2f607fee2ee25ba902e2f2ee4bc675325d119
Feo <34154200+Meadowflaw@users.noreply.github.com>
2020-03-08 13:05:39 -0400

    Update warn.js

--- uBot/commands/moderation/warn.js
+++ uBot/commands/moderation/warn.js
@@ -85,7 +85,8 @@ class WarnCommand extends Command {
               .addField("Warned by user", `${msg.author} with ID: ${msg.author.id}`)
               .addField("Time the user was warned at", msg.createdAt)
               .addField("Reason of warn", reason)
-            .addField("This user's previous punishments in this server", '[link to the API](' + process.env.API_ENDPOINT + '/punish?server_id=' + msg.guild.id + "&" + "user_id=" + member.id + ")");          incidentschannel.send(warnEmbed2);
+            .addField("This user's previous punishments in this server", '[link to the API](' + process.env.API_ENDPOINT + '/punish?server_id=' + msg.guild.id + "&" + "user_id=" + member.id + ")");
+            incidentschannel.send(warnEmbed2);
         }
       }
     }
