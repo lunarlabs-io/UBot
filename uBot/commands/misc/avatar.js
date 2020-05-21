a9dc764501812f20c43631c5d68b1becff09a443
richrobber2 <richardrgguno@hotmail.com>
2020-05-21 05:06:59 -0700

    v12

--- uBot/commands/misc/avatar.js
+++ uBot/commands/misc/avatar.js
@@ -1,30 +1,33 @@
-const Discord = require("discord.js");
-/* eslint-disable linebreak-style */
-const { Command } = require("discord.js-commando");
-class AvatarCommand extends Command {
-  constructor(client) {
-    super (client, {
-      name: "avatar",
-      aliases: [],
-      group: "misc",
-      memberName: "avatar",
-      description: "Look up a users Avatar",
-      ownerOnly: false,
-      guildOnly: false
-      
-    });
-  }
-  async run(msg) {
-    const member = msg.mentions.users.first() || msg.author;
-    if (member) {
-      const embed = new Discord.RichEmbed()
-        .setImage(member.avatarURL)
-        .setColor("#000FFF")
-      // eslint-disable-next-line max-len
-        .addField("Here is the mentioned user's profile picture,", member)
-        .setFooter("Command handled by UBot");
-      msg.channel.send(embed);
+
+const { Command } = require('discord.js-commando')
+const Discord = require('discord.js')
+
+module.exports = class AvatarCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name:"avatar",
+            aliases: [""],
+            group: 'misc',
+            memberName: 'avatar',
+            description: 'Sends the avatar of a user ',
+            args: [
+                {
+                    type:"user",
+                    prompt:"Which user would you like to get the avatar of?",
+                    key:"user",
+                    default: msg => msg.author
+                }
+            ]
+        })
     }
-  }
+    run(msg, { user }) {
+
+        const avatar = new Discord.MessageEmbed()
+        .setImage(user.avatarURL({ dynamic: true, size: 4096 }))
+        .setColor("RANDOM")
+        .addField("Here is the mentioned user's profile picture,", user)
+        .setFooter("Command handled by UBot");
+        msg.channel.send(avatar);
+    
+    }
 }
-module.exports = AvatarCommand;
